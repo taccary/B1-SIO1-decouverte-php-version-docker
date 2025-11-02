0ef3767825dfed663df263b0b0a371aaa713b900
Tiphaine Accary-Barbier <34951152+taccary@users.noreply.github.com>
2025-11-02 12:56:45 +0000

    tests xdebug et renommage

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM php:8.1-rc-apache
+FROM php:8.3-apache
 
 RUN apt-get update && apt-get install -y \
     libfreetype6-dev \
@@ -10,3 +10,11 @@ RUN apt-get update && apt-get install -y \
 COPY . /var/www/html
 COPY custom-php.ini /usr/local/etc/php/conf.d/
 RUN chown -R www-data:www-data /var/www/html
+
+RUN apt-get update && apt-get install -y \
+        zip unzip git libzip-dev \
+    && pecl install xdebug \
+    && docker-php-ext-enable xdebug \
+    && rm -rf /var/lib/apt/lists/*
+
+COPY ./xdebug.ini /usr/local/etc/php/conf.d/xdebug.ini
--- site/retrogamein/BDD/infosConnexionBDD.inc.php
+++ site/retrogamein/BDD/infosConnexionBDD.inc.php
@@ -4,7 +4,7 @@
  */
     $_ENV["username"] = "retrogamein-web"; // utilisateur de la base de données
     $_ENV["password"] = "p@ssw0rd"; // mot de passe de l'utilisateur de la base de données
-    $_ENV["host"] = "db"; // hôte de la base de données
+    $_ENV["host"] = "mysql"; // hôte de la base de données
     $_ENV["port"] = "3306"; // port de la base de données
     $_ENV["dbname"] = "retrogamein"; // nom de la base de données
     $_ENV["dsn"] = "mysql:host=".$_ENV["host"].";dbname=".$_ENV["dbname"].";port=".$_ENV["port"]; // data source name
